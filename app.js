2239758ccee4e573c0ee90c3381b87c297c01fe4
Heesun <68651370+HeesunParknl@users.noreply.github.com>
2020-08-22 11:33:35 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const para = document.querySelector("#chris");
+
+para.addEventListener("click", updateName);
+
+function updateName() {
+    let name = prompt("Enter a new name");
+    para.textContent = "Player 1: " + name;
+}
